5f0726dd69f30831333cc4504566580fd446400d
sapd <sapd@vccloud.vn>
2017-03-03 22:24:43 +0700

    MOD: send use subnet instead of IP

--- send_metric.py
+++ send_metric.py
@@ -8,9 +8,8 @@ class InfluxDB(object):
     Connect to influxdb and send metric
     """
 
-    def __init__(self, host, port, username, password,
-                 database, metrics, path, data_center
-                 ):
+    def __init__(self, host, port, username,
+                 password, database,):
         """
         Init
         """
@@ -20,9 +19,6 @@ class InfluxDB(object):
         self.password = password
         self.database = database
         self.influx = self.get_influxdb_client()
-        self.metrics = metrics
-        self.path = path
-        self.data_center = data_center
 
     def get_influxdb_client(self):
         """
@@ -33,21 +29,21 @@ class InfluxDB(object):
                               username=self.username, password=self.password,
                               database=self.database)
 
-    def send_metric(self):
+    def send_metric(self, metrics, path, data_center):
         """
         Send metric to database
         """
         metrics = []
         # build metric
-        for m in self.metrics:
+        for m in metrics:
             metrics.append({
-                "measurement": self.path,
+                "measurement": path,
                 "tags": {
-                    "data_center": self.data_center,
-                    "ip": m['ip']
+                    "data_center": data_center,
+                    "subnet": m['subnet']
                 },
                 "fields": {
-                    "value": m['time']
+                    "value": float(m['avg_time'])
                 }
             })
         self.influx.write_points(metrics)
